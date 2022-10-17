8917890c1ad7c0a7dafe6837a203e72a5875299e
IngRodro <rr0213032019@unab.edu.sv>
2022-10-16 22:04:15 -0600

    Commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,26 @@
+const {
+  APP_PORT,
+  APP_DATABASE_URL,
+  APP_DATABASE_URL_TEST,
+  APP_CLOUD_NAME,
+  APP_API_KEY,
+  APP_API_SECRET,
+  NODE_ENV,
+  TOKEN_SECRET,
+} = process.env;
 const config = () => ({
-  port: process.env.PORT || 8080,
+  port: APP_PORT || 8080,
   database: {
-    uri: process.env.APP_DATABASE_URL,
+    uri: NODE_ENV === 'test' ? APP_DATABASE_URL_TEST : APP_DATABASE_URL,
     options: {},
+  },
+  imageCloud: {
+    cloud_name: APP_CLOUD_NAME,
+    api_key: APP_API_KEY,
+    api_secret: APP_API_SECRET,
+  },
+  Token: {
+    secret: TOKEN_SECRET,
   },
 });
 
