2669cfc4284f712d29e52a202e6ea9b0ee56fd10
IngRodro <rr0213032019@unab.edu.sv>
2022-10-16 22:35:37 -0600

    Commit

--- src/Application/v1/index.js
+++ src/Application/v1/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import { TokenValidation } from 'Utils/Authentication';
+import restaurantRoutes from './restaurant/restaurant.route';
+import productRoutes from './product/product.route';
+import menuRoutes from './menuOptions/menuOptions.route';
+import userRoutes from './user/user.route';
 
 const router = express.Router();
+
+router.use('/restaurants', restaurantRoutes);
+router.use('/products', TokenValidation, productRoutes);
+router.use('/menu', TokenValidation, menuRoutes);
+router.use('/users', userRoutes);
 
 export default router;
--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = () => ({
   port: process.env.APP_PORT || 8080,
   database: {
-    uri: process.env.NODE_ENV === 'test' ? process.env.APP_DATABASE_URL_TEST : process.env.APP_DATABASE_URL,
+    uri: process.env.APP_DATABASE_URL,
     options: {},
   },
   imageCloud: {
